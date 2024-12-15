83e75b463d0969fb7efb823a4bd389559e6de95f
Lin Pan <linpan0328@gmail.com>
2024-12-15 00:08:41 -0500

    Change stuff

--- src/main/kotlin/org/shockwave/RobotContainer.kt
+++ src/main/kotlin/org/shockwave/RobotContainer.kt
@@ -2,15 +2,12 @@ package org.shockwave
 
 import edu.wpi.first.wpilibj.DriverStation
 import edu.wpi.first.wpilibj2.command.InstantCommand
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController
 import org.shockwave.subsystem.elevator.ElevatorIOSpark
 import org.shockwave.subsystem.elevator.ElevatorSubsystem
 import org.shockwave.utils.Tab
 
 object RobotContainer {
   val elevator = ElevatorSubsystem(ElevatorIOSpark())
-
-  val driverController = CommandXboxController(GlobalConstants.DRIVER_CONTROLLER_PORT)
 
   init {
     DriverStation.silenceJoystickConnectionWarning(true)
